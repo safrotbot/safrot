79497bd890e252a11a100e45f6a14f3fe68703ab
♯ЅᗩFᏒOT꙯ <168010708+safrotbob2@users.noreply.github.com>
2024-05-08 14:29:48 +0300

    Delete plugins directory
    Signed-off-by: ♯ЅᗩFᏒOT꙯ <168010708+safrotbob2@users.noreply.github.com>

--- plugins/owner-leavegc.js
+++
@@ -1,7 +0,0 @@
-let handler = async (m, { conn, args, command }) => {
-await m.reply('*لن اشتاك لكم معه السلامه الي باعنا خسر دلعنا ⁦^⁠_⁠^⁩*') 
-await  conn.groupLeave(m.chat)}
-handler.command = /^(out|leavegc|اخرج|برا)$/i
-handler.group = true
-handler.rowner = true
-export default handler
